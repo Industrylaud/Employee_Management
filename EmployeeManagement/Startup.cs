ffa2d193b7b01056372ca66174018512e218a589
Industrylaud <mrfros22@gmail.com>
2020-03-18 14:35:35 +0100

    fix mvc options and routing learn
    fix mvc options in startup.cs

--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -6,6 +6,7 @@ using EmployeeManagement.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,8 +26,12 @@ namespace EmployeeManagement
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc(options =>
+            {
+                options.EnableEndpointRouting = false;
+            });
             services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,12 +43,15 @@ namespace EmployeeManagement
             }
 
             app.UseStaticFiles();
-            app.UseRouting();
+            //app.UseMvcWithDefaultRoute();
+            app.UseMvc(routes => { routes.MapRoute("default", "{controller=Home}/{Action=Index}/{id?}"); });
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}");
-            });
+            //app.UseRouting();
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}");
+            //});
         }
     }
 }
