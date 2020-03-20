e5e43d6a26b28842fe9cfc798f1772c79a3ac140
Industrylaud <mrfros22@gmail.com>
2020-03-20 13:35:09 +0100

    form validation learn

--- EmployeeManagement/Models/Employee.cs
+++ EmployeeManagement/Models/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,13 @@ namespace EmployeeManagement.Models
     public class Employee
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(50, ErrorMessage = "You have too long name, sorry bro")]
         public string Name { get; set; }
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", 
+            ErrorMessage = "invalid email address")]
+        [Display(Name = "Office email")]
         public string Email { get; set; }
         public Dept Department { get; set; }
     }
