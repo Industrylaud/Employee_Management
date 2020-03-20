c60e6bbc42ddc38aead5aba8974783bb28e1b0b6
Industrylaud <mrfros22@gmail.com>
2020-03-20 13:47:42 +0100

    Select list validation learn

--- EmployeeManagement/Models/Employee.cs
+++ EmployeeManagement/Models/Employee.cs
@@ -9,14 +9,18 @@ namespace EmployeeManagement.Models
     public class Employee
     {
         public int Id { get; set; }
+
         [Required]
         [MaxLength(50, ErrorMessage = "You have too long name, sorry bro")]
         public string Name { get; set; }
+
         [Required]
         [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", 
             ErrorMessage = "invalid email address")]
         [Display(Name = "Office email")]
         public string Email { get; set; }
-        public Dept Department { get; set; }
+
+        [Required]
+        public Dept? Department { get; set; }
     }
 }
