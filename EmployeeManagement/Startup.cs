2658b56b60b9c74096a7be5e55b468e875e2b281
Industrylaud <mrfros22@gmail.com>
2020-03-21 16:28:50 +0100

    database migration learn

--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -27,8 +27,7 @@ namespace EmployeeManagement
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContextPool<AppDbContext>(
-                options => options.UseSqlServer(_config.GetConnectionString("EmployeeDBConnection")));
+            services.AddDbContextPool<AppDbContext>(options => options.UseSqlServer(_config.GetConnectionString("EmployeeDBConection")));
 
             services.AddMvc(options => options.EnableEndpointRouting = false);
             services.AddScoped<IEmployeeRepository, SQLEmployeeRepository>();
